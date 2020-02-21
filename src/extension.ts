381adbee86f53fd80225ed8e7c5a20cda1dda63c
marton.balint <marton.balint@jura.hu>
2020-02-21 18:39:16 +0100

    minor updates, not yet functional

--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,101 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+class XRefEntry {
+	public offset: number;
+	public objectNumber: number;
+
+	constructor(offset: number, objectNumber: number) {
+		this.offset = offset;
+		this.objectNumber = objectNumber;
+	}
+}
+
+function ParseXrefLine(text: String) : XRefEntry {
+	let words = text.split(" ");
+	return new XRefEntry(parseInt(words[0]), parseInt(words[1]));
+}
+
+function ParseXref(text: String) : XRefEntry[] {
+	let entries = new Array<XRefEntry>();
+
+	let lines = text.split(/\r\n|\r|\n/);
+
+	for(let line in lines) {
+
+	}
 
 
+	return entries;
+}
 
+class PdfParser {
+	public ParsePdf(document: vscode.TextDocument) {
+		console.log('parse start');
+
+
+		let text = document.getText();
+
+		// expected something like:
+		// << /Size 6091 /Root 6090 0 R >>
+		// startxref
+		// 1040763
+		// %%EOF
+		let lastFewLines = text.slice(text.length - 50);
+
+		let startxrefToken = "startxref";
+		let startxrefTokenPosition = lastFewLines.search(startxrefToken);
+		
+		
+		let xrefPosition = parseInt(lastFewLines.slice(startxrefTokenPosition + startxrefToken.length));
+
+		let xrefAsString = text.slice(xrefPosition);
+		
+		console.log(xrefPosition);
+	}
+
+	public GetLocation(position: vscode.Position) : vscode.ProviderResult<vscode.Location> {
+
+
+		return null;
+	}
+}
 
 class PdfDefinitionProvider implements vscode.DefinitionProvider {
+	// TODO place into a dictionary for each document
+	private parser: PdfParser;
+    private _disposable: vscode.Disposable;
+
+	constructor() {
+		this.parser = new PdfParser();
+
+		let subscriptions: vscode.Disposable[] = [];
+		vscode.window.onDidChangeActiveTextEditor(this.OnDocumentChanged, this, subscriptions);
+		this._disposable = vscode.Disposable.from(...subscriptions);
+
+		//this._disposable = vscode.window.onDidChangeActiveTextEditor(this.OnDocumentChanged, this);
+		this.OnDocumentChanged();
+	}
+
+	private OnDocumentChanged() {
+		console.log('OnDocumentChanged');
+		let document = vscode.window.activeTextEditor ? vscode.window.activeTextEditor.document : null;
+		if(!document) {
+			return;
+		}
+
+		if(!document.fileName.endsWith("pdf")) {
+			return;
+		}
+
+		this.parser.ParsePdf(document);
+	}
+
+	public dispose() {
+        this._disposable.dispose();
+    }
+	
+	
     public provideDefinition(
 		document: vscode.TextDocument, 
 		position: vscode.Position, 
@@ -15,15 +105,17 @@ class PdfDefinitionProvider implements vscode.DefinitionProvider {
 
 		let tokenPosition = new vscode.Position(position.line + 1, 0);
 
-		console.log('Meh!!!');
-		
-		return new vscode.Location(document.uri, tokenPosition);
+		return this.parser.GetLocation(position);
+		//console.log('Meh!!!');
+		//
+		//return new vscode.Location(document.uri, tokenPosition);
     }
 }
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+
 
 	context.subscriptions.push(
         vscode.languages.registerDefinitionProvider(
